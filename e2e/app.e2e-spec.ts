19499dce7ed4a103916aec27045bd6eef6cea415
Alex Efimov <alex.d.efimov@gmail.com>
2017-01-06 01:36:49 +0300

    Initial commit

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { GooglePOIPage } from './app.po';
+
+describe('google-poi App', function() {
+  let page: GooglePOIPage;
+
+  beforeEach(() => {
+    page = new GooglePOIPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
